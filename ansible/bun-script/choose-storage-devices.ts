2b6bce73d76ddf4941ab4ca315f126ba73c2f9ac
Dean Shub <deanshub@gmail.com>
2025-09-23 09:25:26 +0000

    show usb partitions

--- ansible/bun-script/choose-storage-devices.ts
+++ ansible/bun-script/choose-storage-devices.ts
@@ -2,7 +2,7 @@ import prompts from "prompts";
 import si from "systeminformation";
 
 async function main() {
-  const devices = await getAllUSBDevices();
+  const devices = await getAllUSBPartitions();
   if (devices.length === 0) {
     console.log("No USB devices found");
     return "";
@@ -19,11 +19,17 @@ async function main() {
   console.log(response.devices);
 }
 
-async function getAllUSBDevices() {
+async function getAllUSBPartitions() {
   const blockDevices = await si.blockDevices();
 
   const devices = blockDevices
-    .filter((device) => device.type === "usb")
+    //.filter((device) => device.protocol === "usb")
+    .filter((device) => device.type === 'part' &&
+	    device.device &&
+	    blockDevices.find(parent =>
+    		parent.device === device.device && parent.protocol === 'usb'
+  	    )
+    )
     .map((device) => ({
       name: device.name,
       id: device.uuid,
